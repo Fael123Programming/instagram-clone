2daeeb74ecd825501033381db61420accfef3e74
Fael123Programming <rafaelfonseca1020@gmail.com>
2024-01-01 18:40:58 -0300

    Feat.: backend functions

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -7,19 +7,32 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore} = require("firebase-admin/firestore");
+const {onDocumentCreated, onDocumentDeleted} = require("firebase-functions/v2/firestore");
+const { ServerValue } = require("firebase-admin/database");
 
-const admin = require('firebase-admin');
-admin.initializeApp();
+initializeApp();
 
-const db = admin.firestore();
+exports.addLike = onDocumentCreated('/posts/{creatorId}/userPosts/{postId}/likes/{userId}', async (event) => {
+    await getFirestore()
+    .collection('posts')
+    .doc(event.params.creatorId)
+    .collection('userPosts')
+    .doc(event.params.postId)
+    .update({
+        likesCount: ServerValue.increment(1)
+    });
+});
 
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.removeLike = onDocumentDeleted('/posts/{creatorId}/userPosts/{postId}/likes/{userId}', async (event) => {
+    await getFirestore()
+    .collection('posts')
+    .doc(event.params.creatorId)
+    .collection('userPosts')
+    .doc(event.params.postId)
+    .update({
+        likesCount: ServerValue.increment(-1)
+    });
+});
